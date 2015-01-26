c990205b3c75b475d3a4d8f4592330c4fca7c12a
Daniel Ourada <daniel.ourada@gmail.com>
2015-01-26 11:54:59 -0800

    initial code checkin

---
+++ REST.CSharp/CSharpForm.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace REST.CSharp
+{
+    partial class CSharpForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMerchantId = new System.Windows.Forms.TextBox();
+            this.txtEndPoint = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRequest = new System.Windows.Forms.TextBox();
+            this.txtResponse = new System.Windows.Forms.TextBox();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblResponse = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnProcessReturnFromResponse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MerchantId:";
+            // 
+            // txtMerchantId
+            // 
+            this.txtMerchantId.Location = new System.Drawing.Point(96, 16);
+            this.txtMerchantId.Name = "txtMerchantId";
+            this.txtMerchantId.Size = new System.Drawing.Size(215, 20);
+            this.txtMerchantId.TabIndex = 1;
+            this.txtMerchantId.Text = "019588466313922";
+            // 
+            // txtEndPoint
+            // 
+            this.txtEndPoint.Location = new System.Drawing.Point(96, 70);
+            this.txtEndPoint.Name = "txtEndPoint";
+            this.txtEndPoint.Size = new System.Drawing.Size(215, 20);
+            this.txtEndPoint.TabIndex = 3;
+            this.txtEndPoint.Text = "https://w1.mercurycert.net/PaymentsAPI";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "EndPoint:";
+            // 
+            // txtRequest
+            // 
+            this.txtRequest.Location = new System.Drawing.Point(16, 119);
+            this.txtRequest.Multiline = true;
+            this.txtRequest.Name = "txtRequest";
+            this.txtRequest.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtRequest.Size = new System.Drawing.Size(256, 255);
+            this.txtRequest.TabIndex = 4;
+            // 
+            // txtResponse
+            // 
+            this.txtResponse.Location = new System.Drawing.Point(310, 119);
+            this.txtResponse.Multiline = true;
+            this.txtResponse.Name = "txtResponse";
+            this.txtResponse.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResponse.Size = new System.Drawing.Size(256, 255);
+            this.txtResponse.TabIndex = 5;
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(389, 48);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(177, 23);
+            this.btnProcess.TabIndex = 6;
+            this.btnProcess.Text = "Process Credit Sale";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(389, 12);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(177, 23);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Request:";
+            // 
+            // lblResponse
+            // 
+            this.lblResponse.AutoSize = true;
+            this.lblResponse.Location = new System.Drawing.Point(307, 103);
+            this.lblResponse.Name = "lblResponse";
+            this.lblResponse.Size = new System.Drawing.Size(58, 13);
+            this.lblResponse.TabIndex = 9;
+            this.lblResponse.Text = "Response:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(96, 41);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(215, 20);
+            this.txtPassword.TabIndex = 11;
+            this.txtPassword.Text = "xyz";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Password:";
+            // 
+            // btnProcessReturnFromResponse
+            // 
+            this.btnProcessReturnFromResponse.Location = new System.Drawing.Point(389, 77);
+            this.btnProcessReturnFromResponse.Name = "btnProcessReturnFromResponse";
+            this.btnProcessReturnFromResponse.Size = new System.Drawing.Size(177, 23);
+            this.btnProcessReturnFromResponse.TabIndex = 12;
+            this.btnProcessReturnFromResponse.Text = "Process Return From Response";
+            this.btnProcessReturnFromResponse.UseVisualStyleBackColor = true;
+            this.btnProcessReturnFromResponse.Click += new System.EventHandler(this.btnProcessReturnFromResponse_Click);
+            // 
+            // CSharpForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 434);
+            this.Controls.Add(this.btnProcessReturnFromResponse);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblResponse);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.txtResponse);
+            this.Controls.Add(this.txtRequest);
+            this.Controls.Add(this.txtEndPoint);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMerchantId);
+            this.Controls.Add(this.label1);
+            this.Name = "CSharpForm";
+            this.Text = "REST.CSharp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMerchantId;
+        private System.Windows.Forms.TextBox txtEndPoint;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRequest;
+        private System.Windows.Forms.TextBox txtResponse;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblResponse;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnProcessReturnFromResponse;
+    }
+}
+
---
+++ REST.CSharp/CSharpForm.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RestSharp;
+using System.Web.Script.Serialization;
+
+namespace REST.CSharp
+{
+    public partial class CSharpForm : Form
+    {
+        public CSharpForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            //convert merchantid and password into base64 encoded data
+            var dataByte = UTF8Encoding.ASCII.GetBytes(txtMerchantId.Text + ":" + txtPassword.Text);
+            var merchantIdData = Convert.ToBase64String(dataByte);
+
+            //use a somewhat unique string for invoice number below
+            var dt = DateTime.Now.ToString("yyyyddMMHHmmss");
+
+            //assume data from a swiped keyboard emulation device
+            var swipedFields = ParseDynamagData("%B5499000090006781^TEST/MPS^15120000000000000?;5499000090006781=15120000000000000000?|0600|9A1F82EDFEADADA1F8C9CCC6B9A9C349A33FD9238FAD46082EB9B1E18EFF2A57722104F9149766A9B675D69A8DD7EEF8|425DDE3C92ECB491B6417E78725C9EAB62D0B4D8584A74F0B31945253B71617527051C3CDE595F90||61401000|B157C4CB5DED36E5E643C690F94B84C25571D0B9CA731CB64237AB6D6E185A83DF9F47E639DA3FD9A2C201D6273DF7A271DE2F33FA9F8C87|B047038101811AA|B2914BC9BCC1FAA4|9012090B0470380000B8|D1ED||1000");
+            var encryptedBlock = string.Empty;
+            var encryptedKey = string.Empty;
+            var accountType = string.Empty;
+
+            if (swipedFields.ContainsKey("3") && swipedFields.ContainsKey("9"))
+            {
+                encryptedBlock = swipedFields["3"];
+                encryptedKey = swipedFields["9"];
+                accountType = "Swiped";
+            }
+
+            //create the credit sale request
+            dynamic data = new
+            {
+                InvoiceNo = dt,
+                Memo = "Your POS Name and Version",
+                Purchase = "1.09",
+                Frequency = "OneTime",
+                RecordNo = "RecordNumberRequested",
+                RefNo = dt,
+                EncryptedBlock = encryptedBlock,
+                EncryptedFormat = "MagneSafe",
+                EncryptedKey = encryptedKey,
+                OperatorID = "Current OperatorId",
+                AccountSource = accountType
+            };
+
+            txtRequest.Text = data.ToString();
+
+            var client = new RestClient(txtEndPoint.Text);
+            var request = new RestRequest("Credit/Sale", Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader("Authorization", "Basic " + merchantIdData);
+            request.AddBody(data);
+            var response = client.Execute(request);
+
+            txtResponse.Text = response.Content;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtResponse.Text = string.Empty;
+            txtRequest.Text = string.Empty;
+        }
+
+        private void btnProcessReturnFromResponse_Click(object sender, EventArgs e)
+        {
+            //convert merchantid and password into base64 encoded data
+            var dataByte = UTF8Encoding.ASCII.GetBytes(txtMerchantId.Text + ":" + txtPassword.Text);
+            var merchantIdData = Convert.ToBase64String(dataByte);
+
+            //deserialize the text data returned from REST endpoint into a usable object
+            var serializer = new JavaScriptSerializer();
+            dynamic item = serializer.Deserialize<object>(txtResponse.Text);
+
+            //create the Credit Return
+            dynamic data = new
+            {
+                InvoiceNo = item["InvoiceNo"],
+                Memo = "Your POS Name and Version",
+                Purchase = item["Purchase"],
+                Frequency = "OneTime",
+                RecordNo = item["RecordNo"],
+                RefNo = item["RefNo"],
+                OperatorID = "Current OperatorId",
+            };
+
+            txtRequest.Text = data.ToString();
+
+            var client = new RestClient(txtEndPoint.Text);
+            var request = new RestRequest("Credit/ReturnByRecordNo", Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader("Authorization", "Basic " + merchantIdData);
+            request.AddBody(data);
+            var response = client.Execute(request);
+
+            txtResponse.Text = response.Content;
+
+        }
+
+        private Dictionary<string, string> ParseDynamagData(string swipeData)
+        {
+            try
+            {
+                Dictionary<string, string> fields = new Dictionary<string, string>();
+                string[] firstSplit = swipeData.Split('|');
+                for (int i = 0; i < firstSplit.Length; i++)
+                {
+                    fields.Add(Convert.ToString(i), firstSplit[i]);
+                }
+                return fields;
+            }
+            catch
+            {
+                return new Dictionary<string, string>();
+            }
+        }
+
+
+    }
+}
